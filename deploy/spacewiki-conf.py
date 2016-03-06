8ad3898fba7cea752f2bb721a3b948395697ff7f
Patrick O'Doherty <p@trickod.com>
2016-03-05 22:21:56 -0800

    Half-done port of spacewiki deploy routine
    Copy the spacewiki deploy and make routine to deploy library to ratchet.
    TODO: Add in TLS specific nginx configuration w/ letsencrypt support
    TODO: Add in cron for renewing the LE certificate ever ~60 days

---
+++ deploy/spacewiki-conf.py
@@ -0,0 +1,6 @@
+DATABASE = 'sqlite:////srv/spacewiki/data/db.sqlite3'
+SITE_NAME = 'Noisebridge'
+INDEX_PAGE = 'main_page'
+UPLOAD_PATH = '/srv/spacewiki/data/uploads'
+
+ADMIN_EMAILS = ['tdfischer@hackerbots.net']
